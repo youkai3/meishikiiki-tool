a8a23370fe289145dd667a01559602f026912f6f
youkai3 <kai.hm03@gmail.com>
2026-08-12 00:35:14 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,58 +1,49 @@
-const CACHE_NAME = "meishikiiki-tool-v1";
+const CACHE_NAME = "meishikiiki-tool-v2";
 
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        ).then(() => self.clients.claim())
-    );
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
 
-                return fetch(event.request)
-                    .then(response => {
-                        if (
-                            !response ||
-                            response.status !== 200 ||
-                            response.type === "opaque"
-                        ) {
-                            return response;
-                        }
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseClone);
+          });
 
-                        const responseClone = response.clone();
-
-                        caches.open(CACHE_NAME).then(cache => {
-                            cache.put(event.request, responseClone);
-                        });
-
-                        return response;
-                    })
-                    .catch(() => caches.match("./index.html"));
-            })
-    );
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(cachedResponse => {
+            return cachedResponse || caches.match("./index.html");
+          });
+      })
+  );
 });
